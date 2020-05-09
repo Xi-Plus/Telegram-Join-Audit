4b0cfb91520567e9299aecd822b7c7d8f9428c38
xiplus <huangxuanyuxiplus@gmail.com>
2020-05-09 14:46:16 +0800

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -1,15 +1,15 @@
-CENSORED_CHAT_ID = -1001234567890
-ADMIN_CHAT_ID = -1001234567891
+CENSORED_CHAT_ID = -1000000000000
+ADMIN_CHAT_ID = -1000000000000
 
 DEBUG_MODE = False
 
 DB_HOST = 'localhost'
 DB_USER = ''
 DB_PASS = ''
-DB_DB = 'wikipedia_pol_join'
+DB_DB = 'Telegram-Join-Audit'
 
 TG_TOKEN = ''
-TG_WEBHOOK_URL = 'https://example.com/wikipedia_pol_join/webhook'
+TG_WEBHOOK_URL = 'https://example.com/Telegram-Join-Audit/webhook'
 TG_WEBHOOK_MAX_CONNECTIONS = 5
 
 QUESTIONS = [
