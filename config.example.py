cd7c0ecf5cf0658d09b36a206070577550326f7e
xiplus <huangxuanyuxiplus@gmail.com>
2020-05-09 11:44:40 +0800

    Create config.example.py

---
+++ config.example.py
@@ -0,0 +1,20 @@
+CENSORED_CHAT_ID = -1001234567890
+ADMIN_CHAT_ID = -1001234567891
+
+DB_HOST = 'localhost'
+DB_USER = ''
+DB_PASS = ''
+DB_DB = 'wikipedia_pol_join'
+
+TG_TOKEN = ''
+TG_WEBHOOK_URL = 'https://example.com/wikipedia_pol_join/webhook'
+TG_WEBHOOK_MAX_CONNECTIONS = 5
+
+QUESTIONS = [
+    [
+        '問題1-1',
+        '問題1-2',
+    ], [
+        '問題2',
+    ],
+]
